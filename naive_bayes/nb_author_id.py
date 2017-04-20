4a8df4913dd55d9b2f58313a7a490f191a5fea31
Anmol Shrivastava <s.anmol1994@gmail.com>
2017-04-20 16:37:53 +0530

    Quiz Two - check training and testing(predict) time

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -28,8 +28,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
-print (clf.predict(features_test))
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
 print(clf.score(features_train, labels_train))
 
 
